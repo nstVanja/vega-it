4754bd9bfaef1bb1b50669d999b4ca86ca400073
Vanja <32024620+nstVanja@users.noreply.github.com>
2022-08-03 02:29:38 +0200

    Add files via upload

---
+++ logs-order.js
@@ -0,0 +1,10 @@
+(function () {
+  console.log(1);
+  setTimeout(function () { console.log(2) }, 0);
+  process.nextTick(() => {
+      process.nextTick(console.log.bind(console, 3));
+  });
+  setTimeout(function () { console.log(4) }, 100);
+  process.nextTick(function () { console.log(5) });
+  console.log(6);
+})();
